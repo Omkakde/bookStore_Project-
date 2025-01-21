204f40566f396bc6d32962f9aee00c35d4b5bf0c
Omkakde <kakadeom123@gmail.com>
2025-01-21 14:36:31 +0530

    [Om] Added :Swagger Api Docs to all routes

--- src/routes/book.route.ts
+++ src/routes/book.route.ts
@@ -13,12 +13,120 @@ class bookRoutes {
     }
 
     public routes = () => {
-
-        this.router.get('/',this.redisCache.getBooks ,this.bookController.getAll);  
+/**
+ * @openapi
+ * /api/v1/books:
+ *   get:
+ *     tags:
+ *       - Book
+ *     description: Retrieve a list of all books.
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved books.
+ *       400:
+ *         description: Failed to retrieve books or no books found.
+ */
+        this.router.get('/',this.redisCache.getBooks ,this.bookController.getAll);
+        
+        /**
+ * @openapi
+ * /api/v1/books/{id}:
+ *   get:
+ *     tags:
+ *       - Book
+ *     description: Retrieve details of a specific book by its ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the book to retrieve.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved the book details.
+ *       400:
+ *         description: Book not found or error retrieving the book.
+ */
         this.router.get('/:id',this.redisCache.getBookById, this.bookController.getByBookId);  
+
         this.router.delete('/id', userAuth, isAdmin,this.bookController.deleteBook);
+
+        /**
+ * @openapi
+ * /api/v1/books:
+ *   post:
+ *     tags:
+ *       - Book
+ *     description: Create a new book entry.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               description:
+ *                 type: string
+ *                 example: "ninjas"
+ *               discount_price:
+ *                 type: number
+ *                 example: 14.99
+ *               admin_user_id:
+ *                 type: integer
+ *                 example: 5
+ *               book_name:
+ *                 type: string
+ *                 example: "Hollywood"
+ *               author:
+ *                 type: string
+ *                 example: "Salman Khan"
+ *               quantity:
+ *                 type: integer
+ *                 example: 10
+ *               price:
+ *                 type: number
+ *                 example: 2000
+ *     responses:
+ *       201:
+ *         description: Book created successfully.
+ *       400:
+ *         description: Invalid data or authorization error.
+ */
         this.router.post('/',userAuth,this.BookValidator.bookCreateValid ,isAdmin,this.bookController.addBook);
+
+        /**
+ * @openapi
+ * /api/v1/books/{id}/updatePrice:
+ *   put:
+ *     tags:
+ *       - Book
+ *     description: Update the price of a specific book.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the book to update.
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               price:
+ *                 type: number
+ *                 example: 888.00
+ *     responses:
+ *       200:
+ *         description: Book price updated successfully.
+ *       400:
+ *         description: Invalid data or authorization error.
+ */
         this.router.put('/:id/updatePrice',this.BookValidator.bookUpdateValid,userAuth, isAdmin,this.bookController.updatePrice);
+        
         this.router.put('/:id/updateQuantity',userAuth, isAdmin,this.bookController.updateQuantity);
         
     };
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -13,11 +13,152 @@ class UserRoutes {
   }
 
   private routes = () => {
-
+    
+//////////////////////////////////////////////////////////////////////
+ /**
+     * @openapi
+     * /api/v1/users/:
+     *   post:
+     *     tags:
+     *       - User
+     *     summary: register
+     *     description: Allows a user to register.
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:      
+     *               name:
+     *                 type: string
+     *                 example: user1
+     *               email:
+     *                 type: string
+     *                 example: "user1@example.com"
+     *               phone:
+     *                 type: number
+     *                 example: 1234567890
+     *               role:
+     *                 type: string
+     *                 example: user
+     *               password:
+     *                 type: string
+     *                 example: "password123"
+     *     responses:
+     *       200:
+     *         description: user.username registered successfully!.
+     *       400:
+     *         description: user already exist.
+     */
     this.router.post('/',this.UserValidator.registrationValid,this.UserController.newUser);
+
+       /**
+     * @openapi
+     * /api/v1/users/login:
+     *   post:
+     *     tags: 
+     *        - User
+     *     summary: login
+     *     description: Allows a user to login.
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               email:
+     *                 type: string
+     *                 example: "user1@example.com"
+     *               password:
+     *                 type: string
+     *                 example: "password123"
+     *     responses:
+     *       200:
+     *         description: User logged in successfully.
+     *       400:
+     *         description: Invalid email or Invalid email or password.
+     */
     this.router.post('/login',this.UserValidator.loginValid, this.UserController.login);
+
+     /**
+     * @openapi
+     * /api/v1/users/refreshtoken:
+     *   post:
+     *     tags: 
+     *        - User
+     *     summary: refresh token
+     *     description: Allows a user get new access token.
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               refreshtoken:
+     *                 type: string
+     *                 example: ""
+     *     responses:
+     *       200:
+     *         description: newToken:"<token>".
+     *       400:
+     *         description: refreshtoken required or token not found in database.
+     */
     this.router.post('/refreshtoken',this.UserValidator.refreshTokenValid,this.UserController.refreshToken);
+
+    // forget password
+  /**
+     * @openapi
+     * /api/v1/users/forgotpassword:
+     *   post:
+     *     tags: 
+     *        - User
+     *     summary: forgot password
+     *     description: Allows a user get access token in mail.
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               email:
+     *                 type: string
+     *                 example: "user1@example.com"
+     *     responses:
+     *       200:
+     *         description: token sent to mail.
+     *       400:
+     *         description: user not found.
+     */
     this.router.post('/forgotpassword',this.UserValidator.forgotPassValid,this.UserController.forgotPass);
+
+      /**
+     * @openapi
+     * /api/v1/users/resetpassword:
+     *   post:
+     *     tags: 
+     *        - User
+     *     summary: reset password
+     *     description: Allows a user to reset password through access token from mail.
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               newPassword:
+     *                 type: string
+     *                 example: "user123"
+     *     responses:
+     *       200:
+     *         description: your password has been reset.
+     *       400:
+     *         description: could not update password.
+     */
     this.router.post('/resetpassword',this.UserValidator.resetPassValid,userAuth,this.UserController.resetPass);
 
 
